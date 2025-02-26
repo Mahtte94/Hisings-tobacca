56a1c3b01088371f18b76278aba0ab97f1d1badc
JosAhl <jos@Josefine.>
2025-02-26 19:34:30 +0100

    display errors

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,17 +1,24 @@
+<h1>New Product</h1>
 
-  <h1>New Product</h1>
+@if ($errors->any())
+<ul>
+  @foreach ($errors->all() as $error)
+  <li>{{ $error }}</li>
+  @endforeach
+</ul>
+@endif
 
-  <form method="post" action=" {{ route('store')}}">
-    @csrf
+<form method="post" action=" {{ route('store')}}">
+  @csrf
 
-    <label for="name">Item name</label>
-    <input type="text" name="name" id="name" value=" {{ old('name')}}">
+  <label for="name">Item name</label>
+  <input type="text" name="name" id="name" value=" {{ old('name')}}">
 
-    <label for="description">Description</label>
-    <textarea name="description" id="description">{{ old('description')}}</textarea>
+  <label for="description">Description</label>
+  <textarea name="description" id="description">{{ old('description')}}</textarea>
 
-    <label for="price">Price</label>
-    <input type="text" name="price" id="price" value="{{ old('price')}}">
+  <label for="price">Price</label>
+  <input type="text" name="price" id="price" value="{{ old('price')}}">
 
-    <button>Create Product</button>
-  </form>
+  <button>Create Product</button>
+</form>

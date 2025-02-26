f303dc9e9683094dccfb2d4d86bfeee637bd2b66
JosAhl <jos@Josefine.>
2025-02-26 18:29:15 +0100

    fixed index route for startup

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,11 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
-
-Route::get('/index', [ProductController::class, 'index'])
-->name('index');
+Route::get('/', [ProductController::class, 'index'])
+    ->name('index');
 
 Route::get('/create', [ProductController::class, 'create'])
-->name('create');
+    ->name('create');
 
 Route::post('/store', [ProductController::class, 'store'])
-->name('store');
-
-
+    ->name('store');

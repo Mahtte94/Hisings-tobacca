a86e660a968acd408fa216c4c795085a0477d3eb
JosAhl <josefineahlstrand@live.se>
2025-03-04 23:23:04 +0100

    added css file

--- resources/views/components/view/layout.blade.php
+++ resources/views/components/view/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hisings Tobacca</title>
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 </head>
 
 <body>
